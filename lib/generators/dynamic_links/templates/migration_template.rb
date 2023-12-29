6d7d1aee478c57f339f151874b18f89c785ebb25
Saiqul Haq <saiqulhaq@gmail.com>
2023-12-30 03:37:43 +0700

    add KGS strategy

---
+++ lib/generators/dynamic_links/templates/migration_template.rb
@@ -0,0 +1,6 @@
+class AddAvailableToShortenedUrls < ActiveRecord::Migration[7.1]
+  def change
+    add_column :dynamic_links_shortened_urls, :available, :boolean, null: false, default: true
+    add_index :dynamic_links_shortened_urls, :available
+  end
+end

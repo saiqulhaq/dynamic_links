3bc3de17c40fb0c0be239c2df5b8d92fcd1e9c1e
Saiqul Haq <saiqulhaq@gmail.com>
2024-01-01 14:07:19 +0700

    fix: REST API to shorten a link

--- app/controllers/dynamic_links/v1/short_links_controller.rb
+++ app/controllers/dynamic_links/v1/short_links_controller.rb
@@ -1,9 +1,14 @@
 class DynamicLinks::V1::ShortLinksController < ApplicationController
-    short_url = DynamicLinks.shorten(url)
-    render json: {
-      shortLink: short_link.short_url,
-      previewLink: "#{short_link.short_url}?preview=true",
-      warning: []
-    }, status: :created
+  def create
+    url = params.require(:url)
+    # validate url
+    # if !url_shortener.valid_url?(url)
+    #   render json: { error: 'invalid url' }, status: :bad_request
+    #   return
+    # end
+
+    # shorten url
+    # save to db (not implemented yet)
+    render json: DynamicLinks.generate_short_url(url), status: :created
   end
 end

91dace5c5e1fb92383f8aad91edd29337cac266a
Saiqul Haq <saiqulhaq@gmail.com>
2024-01-01 16:23:56 +0700

    fix: improve initialization performance

--- lib/dynamic_links.rb
+++ lib/dynamic_links.rb
@@ -24,16 +24,20 @@ module DynamicLinks
   end
 
   def self.shorten_url(url)
+    @strategy_classes ||= {}
+    strategy_key = configuration.shortening_strategy.to_s
+
     begin
-      strategy_class = "DynamicLinks::ShorteningStrategies::#{configuration.shortening_strategy.to_s.camelize}Strategy".constantize
-      strategy = strategy_class.new
+      @strategy_classes[strategy_key] ||= "DynamicLinks::ShorteningStrategies::#{strategy_key.camelize}Strategy".constantize
+      strategy = @strategy_classes[strategy_key].new
     rescue NameError
-      raise "Invalid shortening strategy: #{configuration.shortening_strategy}"
+      raise "Invalid shortening strategy: #{strategy_key}"
     rescue ArgumentError
-      raise "#{strategy_class} needs to be initialized with arguments"
+      raise "#{@strategy_classes[strategy_key]} needs to be initialized with arguments"
     rescue => e
       raise "Unexpected error while initializing the strategy: #{e.message}"
     end
+
     strategy.shorten(url)
   end
 

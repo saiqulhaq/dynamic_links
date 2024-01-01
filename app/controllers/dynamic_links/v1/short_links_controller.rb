5becd41a867903e4cc04f4c0ae7785b878dfadd8
Saiqul Haq <saiqulhaq@gmail.com>
2024-01-01 18:07:51 +0700

    feat: render unauthorized status when API key is not found

--- app/controllers/dynamic_links/v1/short_links_controller.rb
+++ app/controllers/dynamic_links/v1/short_links_controller.rb
@@ -1,7 +1,12 @@
 class DynamicLinks::V1::ShortLinksController < ApplicationController
   def create
     url = params.require(:url)
-    client = DynamicLinks::Client.find_by!(api_key: params.require(:api_key))
+    client = DynamicLinks::Client.find_by(api_key: params.require(:api_key))
+    unless client
+      render json: { error: 'invalid api key' }, status: :unauthorized
+      return
+    end
+
     render json: DynamicLinks.generate_short_url(url, client), status: :created
   end
 end

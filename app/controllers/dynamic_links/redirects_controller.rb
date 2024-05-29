d55d5d7010c698b1cec3f116b3232d2662da3e12
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-05-30 02:38:06 +0800

    Add ahoy track to redirects_controller

--- app/controllers/dynamic_links/redirects_controller.rb
+++ app/controllers/dynamic_links/redirects_controller.rb
@@ -5,6 +5,12 @@ module DynamicLinks
       link = ShortenedUrl.find_by(short_url: short_url)
 
       if link
+        ahoy.track "ShortenedUrl Visit", {
+          shortened_url: short_url,
+          user_agent: request.user_agent,
+          referrer: request.referrer
+        }
+
         redirect_to link.url, status: :found, allow_other_host: true
       else
         raise ActiveRecord::RecordNotFound

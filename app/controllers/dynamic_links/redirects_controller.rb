6e998a815b598d1c88f59625c274d01a088dd50a
Saiqul Haq <saiqulhaq@gmail.com>
2024-09-04 21:51:30 +0700

    chore: refactor

--- app/controllers/dynamic_links/redirects_controller.rb
+++ app/controllers/dynamic_links/redirects_controller.rb
@@ -1,11 +1,13 @@
 module DynamicLinks
   class RedirectsController < ApplicationController
+
+    # Rails will return a 404 if the record is not found
     def show
       short_url = params[:short_url]
       link = ShortenedUrl.find_by!(short_url: short_url)
 
-      raise ActiveRecord::RecordNotFound if link.expires_at.present? && link.expires_at.past?
-      
+      raise ActiveRecord::RecordNotFound if link.expired?
+
       redirect_to link.url, status: :found, allow_other_host: true
     end
   end

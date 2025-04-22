02d1b832bec722d848328f15ac8dde5c695693a8
Dayu <102780748+dayuhh@users.noreply.github.com>
2025-04-22 16:15:11 +0700

    feat add expand method

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ DynamicLinks::Engine.routes.draw do
   get '/:short_url', to: 'redirects#show', as: :shortened
   namespace :v1 do
     post "/shortLinks", to: "short_links#create", as: :short_links
+    get "/shortLinks/:short_url", to: "short_links#expand", as: :full_url
   end
 end

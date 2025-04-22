d90aaf9ed4541b9dd5abaca00318446e22a25f18
Dayu <102780748+dayuhh@users.noreply.github.com>
2025-04-22 17:03:05 +0700

    fix review code

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ DynamicLinks::Engine.routes.draw do
   get '/:short_url', to: 'redirects#show', as: :shortened
   namespace :v1 do
     post "/shortLinks", to: "short_links#create", as: :short_links
-    get "/shortLinks/:short_url", to: "short_links#expand", as: :full_url
+    get "/shortLinks/:short_url", to: "short_links#expand", as: :expand_short_link
   end
 end

1471c41e4226686a0146156bc270ddbe277b6467
Dayu <102780748+dayuhh@users.noreply.github.com>
2025-05-15 11:24:18 +0700

    fix code and add test

--- test/dummy/config/initializers/dynamic_links.rb
+++ test/dummy/config/initializers/dynamic_links.rb
@@ -7,15 +7,6 @@ DynamicLinks.configure do |config|
                                   DynamicLinks::Configuration::DEFAULT_SHORTENING_STRATEGY
                                 end
   config.enable_rest_api = true
-  if ENV['CITUS_ENABLED'].to_s == 'true'
-    config.db_infra_strategy = :sharding
-  end
-
-  config.redis_counter_config.pool_size = 5
-  config.redis_counter_config.pool_timeout = 5
-  config.redis_counter_config.config = {
-    host: ENV.fetch('REDIS_HOST', 'localhost'),
-    port: ENV.fetch('REDIS_PORT', 6379).to_i,
-    db: ENV.fetch('REDIS_DB', 0).to_i,
-  }
+  config.db_infra_strategy = :standard
+  config.async_processing = false
 end

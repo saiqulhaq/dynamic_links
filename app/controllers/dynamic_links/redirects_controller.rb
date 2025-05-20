40577182e6899605ff40b31559bfa7badba7cdba
Dayu <102780748+dayuhh@users.noreply.github.com>
2025-05-20 16:39:44 +0700

    prevent data short url not found

--- app/controllers/dynamic_links/redirects_controller.rb
+++ app/controllers/dynamic_links/redirects_controller.rb
@@ -11,12 +11,16 @@ module DynamicLinks
       end
 
       short_url = params[:short_url]
-      link = ShortenedUrl.find_by!(short_url: short_url)
+      link = ShortenedUrl.find_by(short_url: short_url)
 
+      if link.nil?
+        url = 'https://k4mu4.app.goo.gl'
+        redirect_to url + "/#{params[:short_url]}", status: :found, allow_other_host: true
+        return
+      end
       raise ActiveRecord::RecordNotFound if link.expired?
 
       redirect_to link.url, status: :found, allow_other_host: true
     end
   end
 end
-

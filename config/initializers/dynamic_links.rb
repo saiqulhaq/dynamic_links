b1a2048bb34e1d0dcf463f424e005a22f44a80d5
Dayu <102780748+dayuhh@users.noreply.github.com>
2025-05-23 16:12:38 +0700

    move config firebase_host and fix default value

--- config/initializers/dynamic_links.rb
+++
@@ -1,8 +0,0 @@
-DynamicLinks.configure do |config|
-  config.shortening_strategy = :nano_id
-  config.enable_rest_api = true
-  config.db_infra_strategy = :standard
-  config.async_processing = false
-  config.enable_fallback_mode = true
-  config.firebase_host = 'https://k4mu4.app.goo.gl'
-end

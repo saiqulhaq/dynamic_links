c4db97496b84b965c15e8675763ee05ae97b7ad9
Dayu <102780748+dayuhh@users.noreply.github.com>
2025-05-26 17:45:33 +0700

    fix test case for redirect and shortlink controller

--- test/controllers/redirections_controller_test.rb
+++ test/controllers/redirections_controller_test.rb
@@ -7,6 +7,14 @@ module DynamicLinks
     setup do
       @valid_hostname = dynamic_links_clients(:one).hostname
       host! @valid_hostname
+
+      @original_fallback_mode = DynamicLinks.configuration.enable_fallback_mode
+      @original_firebase_host = DynamicLinks.configuration.firebase_host
+    end
+
+    teardown do
+      DynamicLinks.configuration.enable_fallback_mode = @original_fallback_mode
+      DynamicLinks.configuration.firebase_host = @original_firebase_host
     end
 
     test "redirects to original URL for valid short URL" do
@@ -49,6 +57,25 @@ module DynamicLinks
 
       assert_response :not_found
       assert_equal 'URL not found', @response.body
+    end
+
+    test "redirects to Firebase host when short URL not found and fallback mode is enabled" do
+      DynamicLinks.configuration.enable_fallback_mode = true
+      DynamicLinks.configuration.firebase_host = "https://k4mu4.app.goo.gl"
+
+      get shortened_url(short_url: "nonexistent123")
+
+      assert_response :found
+      assert_redirected_to "https://k4mu4.app.goo.gl/nonexistent123"
+    end
+
+    test "responds with not found when fallback mode is enabled but firebase host is blank" do
+      DynamicLinks.configuration.enable_fallback_mode = true
+      DynamicLinks.configuration.firebase_host = ""  # no error expected now
+
+      get shortened_url(short_url: "nonexistent123")
+
+      assert_response :not_found
     end
   end
 end

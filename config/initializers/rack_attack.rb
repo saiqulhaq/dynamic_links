10a48c29963b43e880d1bd77703d901a6b9c06b6
Saiqul Haq <saiqulhaq@gmail.com>
2025-08-05 13:32:33 +0700

    fix: adjust throttling logic for trusted IPs in rack_attack initializer

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,9 +3,7 @@ TRUSTED_IPS = ENV.fetch('RACK_ATTACK_TRUSTED_IPS', '').split(',').map(&:strip).f
 
 # Throttle requests for trusted IPs - 1000 requests per second
 Rack::Attack.throttle('requests by trusted ip', limit: 1000, period: 1) do |request|
-  if request.path == '/v1/shortLinks' && request.post? && TRUSTED_IPS.any? && TRUSTED_IPS.include?(request.ip)
-    request.ip
-  end
+  request.ip if request.path == '/v1/shortLinks' && request.post? && !TRUSTED_IPS.include?(request.ip)
 end
 
 # Throttle requests to 5 requests per 2 seconds by IP (for non-trusted IPs)

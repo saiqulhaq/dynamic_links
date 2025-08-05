700c53ac313ee50c8480de8ce904d1df3e1f9d4c
Saiqul Haq <saiqulhaq@gmail.com>
2025-08-05 11:56:05 +0700

    fix: update model class definition and include client in dynamic links resource

--- app/avo/resources/dynamic_links_shortened_url.rb
+++ app/avo/resources/dynamic_links_shortened_url.rb
@@ -1,22 +1,22 @@
 class Avo::Resources::DynamicLinksShortenedUrl < Avo::BaseResource
-  self.model_class = "DynamicLinks::ShortenedUrl"
+  self.model_class = 'DynamicLinks::ShortenedUrl'
   self.title = :short_url
-  self.includes = []
+  self.includes = [:client]
 
   def fields
     field :id, as: :id
     field :client, as: :belongs_to, required: true
-    field :short_url, as: :text, required: true, help: "Short URL identifier"
-    field :url, as: :text, required: true, help: "Original URL to redirect to"
-    field :expires_at, as: :date_time, help: "Optional expiration date"
-    field :created_at, as: :date_time, hide_on: [:new, :edit]
-    field :updated_at, as: :date_time, hide_on: [:new, :edit]
+    field :short_url, as: :text, required: true, help: 'Short URL identifier'
+    field :url, as: :text, required: true, help: 'Original URL to redirect to'
+    field :expires_at, as: :date_time, help: 'Optional expiration date'
+    field :created_at, as: :date_time, hide_on: %i[new edit]
+    field :updated_at, as: :date_time, hide_on: %i[new edit]
 
     field :expired, as: :boolean, computed: true do
       record.expired?
     end
 
-    field :full_short_url, as: :text, computed: true, hide_on: [:new, :edit] do
+    field :full_short_url, as: :text, computed: true, hide_on: %i[new edit] do
       "#{record.client.scheme}://#{record.client.hostname}/#{record.short_url}"
     end
   end

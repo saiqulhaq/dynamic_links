e724c8363c82592174491d22d075a000aced99ce
Saiqul Haq <saiqulhaq@gmail.com>
2025-08-08 12:29:11 +0700

    remove ahoy

--- test/dummy/app/models/ahoy/event.rb
+++
@@ -1,7 +0,0 @@
-class Ahoy::Event < ApplicationRecord
-  include Ahoy::QueryMethods
-  
-  self.table_name = "ahoy_events"
-
-  belongs_to :visit, class_name: "Ahoy::Visit", foreign_key: "ahoy_visits_id"
-end
--- test/dummy/app/models/ahoy/visit.rb
+++
@@ -1,5 +0,0 @@
-class Ahoy::Visit < ApplicationRecord
-  self.table_name = "ahoy_visits"
-
-  has_many :ahoy_events, class_name: "Ahoy::Event"
-end

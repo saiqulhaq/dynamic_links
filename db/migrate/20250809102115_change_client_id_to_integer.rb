536eb4b5c40a347e132980d1f4dadeaaa6839758
Saiqul Haq <saiqulhaq@gmail.com>
2025-08-09 21:41:51 +0700

    Remove migration to change client_id column to integer in dynamic_links_analytics_link_clicks

--- db/migrate/20250809102115_change_client_id_to_integer.rb
+++
@@ -1,5 +0,0 @@
-class ChangeClientIdToInteger < ActiveRecord::Migration[8.0]
-  def change
-    change_column :dynamic_links_analytics_link_clicks, :client_id, :integer, null: false, using: 'client_id::integer'
-  end
-end

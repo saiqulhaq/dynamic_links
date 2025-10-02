ca55c705a3e08138609d62bb57f93fc986920673
Saiqul Haq <saiqulhaq@gmail.com>
2025-10-02 10:14:36 +0700

    init authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Authentication
   # Include performance tracking if ElasticAPM is enabled
   include PerformanceTracking
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   mount_avo
   root to: 'up#databases'
 
